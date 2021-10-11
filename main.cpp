05ab8a2671414ac36348394d2192382ecc1bc5c6
Eric Mandolesi <mandolesi.geo@gmail.com>
2021-10-11 11:42:01 +0100

    add input file reader

--- main.cpp
+++ main.cpp
@@ -13,7 +13,49 @@
  */
 #include <iostream>
 #include "Objects.h"
-int main() {
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <map>
+
+int main(int argc, char* argv[]) {
+    if(argc<2)throw std::runtime_error("This program must be called with config file argument.");
+    std::map<std::string,double> data;
+    // std::ifstream is RAII, i.e. no need to call close
+    std::ifstream cFile (argv[1]);
+    if (cFile.is_open())
+    {
+        std::string line;
+        while(getline(cFile, line)){
+            line.erase(std::remove_if(line.begin(), line.end(), isspace),
+                       line.end());
+            if(line[0] == '#' || line.empty())
+                continue;
+            auto delimiterPos = line.find("=");
+            auto name = line.substr(0, delimiterPos);
+            auto value = line.substr(delimiterPos + 1);
+            data[name] = stod(value);
+        }
+
+    }
+    else {
+        std::cerr << "Couldn't open config file for reading.\n";
+    }
+    Environment myenv;
+    myenv.riskFreeRate = data["risk-free-rate"];
+    myenv.underlyingT0Price = data["S-T0-Price"];
+    myenv.volatility = data["volatility"];
+    myenv.averageDividendsPerYear = data["average-dividends-per-year"];
+
+    CallPut callPut;
+    TradeType type;
+    (data["callput"]>0.)?callPut=CallPut::Call:callPut=CallPut::Put;
+    (data["european"]>0.)?type=TradeType::European:type=TradeType::American;
+    Option myopt(data["strike"], static_cast<int>(data["days-to-maturity"]), type, callPut);
+
+    std::cout << myopt<<"\n";
+    return 0;
     Option option1(60, 365, TradeType::European, CallPut::Call);
     Environment env;
     env.riskFreeRate = 5e-2;
